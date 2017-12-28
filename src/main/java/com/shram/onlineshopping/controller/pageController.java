bcd70975ee4f234ee3930383ae3799198d76f5f5
shramrahul <shreeramchaulagain@gmail.com>
2017-12-28 14:47:45 -0600

    configured the dispatcher-servlet and view resolver

---
+++ src/main/java/com/shram/onlineshopping/controller/pageController.java
@@ -0,0 +1,16 @@
+package com.shram.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class pageController {
+
+    @RequestMapping(value={"/","/home","/index"})
+    public ModelAndView index(){
+        ModelAndView mv= new ModelAndView("page");
+        mv.addObject("greeting","Welcome to Online shopping");
+        return mv;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/page.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shram
+  Date: 12/28/2017
+  Time: 2:31 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>home page</title>
+</head>
+<body>
+    ${greeting}
+</body>
+</html>
